aed60b17522aac482801209633f955fdd8116086
vaco <jan.lenaerts@vale-consultancy.com>
2011-10-18 14:22:28 +0000

    latest code

---
+++ database.php
@@ -0,0 +1,22 @@
+<?php
+
+class MysqlDB {
+
+   protected $_mysql;
+
+
+   public function __construct($host, $username, $password, $db) {
+      $this->_mysql = new mysqli($host, $username, $password, $db) or die('There was a problem connecting to the database');
+   }
+
+
+
+
+
+   public function __destruct() 
+   {
+		$this->_mysql->close();
+   }
+
+}
+
