2a02d3b20854d8f06d78f4e2afddeeeb51507d0c
vaco <jan.lenaerts@vale-consultancy.com>
2011-10-18 08:15:55 +0000

    new file

---
+++ array.php
@@ -0,0 +1,16 @@
+<?php
+
+$personalInfo = array (
+    'name'          =>  'Jan Lenaert',
+    'occuppation'   =>  'PHP Developer',
+    'age'           =>  '26',
+    'location'      =>  'Genk'
+    
+    );
+    
+echo $personalInfo['name'];
+
+?>
+
+
+
