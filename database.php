a3f81b7b9525981e290b8da6d70cd1f6c6dbbf49
vaco <jan.lenaerts@vale-consultancy.com>
2011-10-19 14:38:34 +0000

    latest build

--- database.php
+++ database.php
@@ -5,9 +5,11 @@ class database {
    protected $_mysql;
 
 
-   public function __construct($host, $username, $password, $db) {
-      $this->_mysql = new mysqli($host, $username, $password, $db) or die('There was a problem connecting to the database');
+   public function __construct($host, $username, $password, $database) {
+      $this->_mysql = new mysqli($host, $username, $password, $database) or die('There was a problem connecting to the database');
    }
+   
+   
 
 
 
